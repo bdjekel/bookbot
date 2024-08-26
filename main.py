15712d2d790b4101f5b46847d8d3ae656bd25c11
bdjekel <bdjekel@gmail.com>
2024-08-26 12:10:38 -0500

    update main.py to count characters

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ def main():
     file_location = "books/frankenstein.txt"
     content_string = get_file_content(file_location)
     word_count = count_words(content_string)
-
+    char_count = count_characters(content_string)
     print(f"{word_count} words in document")
+    print(f"{char_count}")
+
 
 def get_file_content(file_path):
     with open(file_path) as f:
@@ -12,5 +14,15 @@ def get_file_content(file_path):
 def count_words(file_contents):
     word_list = file_contents.split()
     return len(word_list)
+
+def count_characters(file_contents):
+    char_counts = {}
+    lowered_contents = file_contents.lower()
+    for i in lowered_contents:
+        if i in list(char_counts.keys()):
+            char_counts[i] += 1
+        else:
+            char_counts[i] = 1
+    return char_counts
     
 main()
