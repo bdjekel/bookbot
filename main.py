8cfe8d772e1b4371e6ca497f7e46f2ba44dfa901
bdjekel <bdjekel@gmail.com>
2024-08-26 12:45:23 -0500

    update main.py to print report

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ def main():
     content_string = get_file_content(file_location)
     word_count = count_words(content_string)
     char_count = count_characters(content_string)
-    print(f"{word_count} words in document")
-    print(f"{char_count}")
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words in document\n")
+    sorted_char_count = generate_report(char_count)
+    for i in sorted_char_count:
+        char = i["character"]
+        count = i["count"]
+        print(f" The '{char}' character was found {count} times")
+    print("--- End report ---")
 
+def generate_report(character_count):
+    def sort_on(character_count):
+        return character_count["count"]
+    
+    sorted_character_counts = []
+    for key, value in character_count.items():
+        if key.isalpha():
+            sorted_character_counts.append({"character": key, "count" : value})
+        else:
+            pass
+    
+    sorted_character_counts.sort(reverse=True, key=sort_on)
+    return sorted_character_counts
 
 def get_file_content(file_path):
     with open(file_path) as f:
